9c88de37b15c726bf846197e8e43a3ed4445b532
eric <eric@pop-os.localdomain>
2021-12-30 11:51:16 -0600

    added basic argument handler for passing in file path to json data and search string

--- search.py
+++ search.py
@@ -7,16 +7,16 @@ def search(data, search_str):
     root = False
     if type(data) == list:
         for i,v in enumerate(data):
-            json_data = json.dumps(v)
+            json_data = json.dumps(v, separators=(',',':'))
             if search_str in json_data: 
                 results.append((i,v))
     elif type(data) == dict:
         for k in data.keys():
             v = data.get(k)
-            if search_str in k:
+            if search_str == k:
                 root = True
                 results.append((k,v))
-            json_data = json.dumps(v)
+            json_data = json.dumps(v, separators=(',',':'))
             if search_str in json_data:
                 results.append((k,v))
     return results, root
@@ -36,9 +36,10 @@ def handler(raw_data, search_str):
     path = []
     data = json.loads(raw_data)
     limit = 100
-    while search_str in json.dumps(data):
+    while search_str in json.dumps(data, separators=(',',':')):
         limit -= 1
         results, root = search(data, search_str)
+        #print(f'results: {results}')
         if root:
             break
         if len(results) > 1:
@@ -57,50 +58,22 @@ def handler(raw_data, search_str):
 
 if __name__ == "__main__":
 
-    dict_data = {
-      "a": {
-        "b": [
-          "c",
-          "d",
-          "e",
-          {
-            "f": "g",
-            "h": "i"
-          }
-        ]
-      },
-      "j": {
-        "k": {
-          "l": {
-            "m": {
-              "n": [
-                "o",
-                "p",
-                "q",
-                {
-                  "r": "s",
-                  "t": "u",
-                  "v": "w",
-                  "x": [
-                    "y",
-                    "z"
-                  ]
-                }
-              ]
-            }
-          }
-        }
-      }
-    }
+    import sys
 
-    raw_json_data = json.dumps(dict_data)
-    
-    search_string = 'w'
+    input_file = sys.argv[1]
+    search_string = sys.argv[2]
+
+    with open(input_file) as f:
+        raw_json_data = f.read()
+
+    #print(raw_json_data)
+    dict_data = json.loads(raw_json_data)
+    #print(data_dict)
 
     path = handler(raw_json_data, search_string)
 
-    print(f'search for: {search_string}')
+    print(f'search for: \'{search_string}\'')
     print(f'path: {path}')
     print(f'test: eval(data{path})')
-    val = eval(f'{dict_data}{path}')
+    val = json.dumps(eval(f'{dict_data}{path}'), separators=(',',':'))
     print(f'results: {val}')
